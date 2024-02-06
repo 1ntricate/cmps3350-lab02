0a825b290f18aeed9cf58a2deb710159401dd0ba
Nathan Nguyen <nnguyen@odin.cs.csubak.edu>
2024-02-05 17:00:47 -0800

    lab02 changes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
+//Nathan Nguyen
+
 // CMPS 3350 - lab 02
 // Prints each command-line argument passed into the program, one per line
 #include <stdio.h>
+#include <algorithm>
+#include <cstring>
 
 int main(int argc, char ** argv)
 {
-    for(int i = 1; i < argc; i++) {
+    for(int i = argc - 1; i >= 1; i--) {
+        std::reverse(argv[i], argv[i] + strlen(argv[i])); 
         printf("arg %d: %s\n", i, argv[i]);
     }
     return 0;
